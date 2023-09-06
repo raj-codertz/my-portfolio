b1040caaa8beeca99a99d51643d1498dbc0dcde9
suinegraj <rajabujshabani@gmail.com>
2023-09-06 18:31:21 +0300

    added my porfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './components/Hero';
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+
+  return (
+      <div>
+         <Navbar />
+         <Hero />
+      </div>
+  )
+}
+
+export default App
