28f4b6e7f9f6607d3cb8d639b8cad4c80607364f
suinegraj <rajabujshabani@gmail.com>
2023-09-07 21:39:47 +0300

    added styles in contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About';
+import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Navbar from "./components/Navbar.jsx";
 import Skills from './components/Skills';
@@ -14,6 +15,7 @@ function App() {
          <About />
          <Skills />
          <Work />
+         <Contact />
       </div>
   )
 }
