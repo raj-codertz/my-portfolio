56834c12188aabb51c75a80956c19854135d7af6
suinegraj <rajabujshabani@gmail.com>
2023-09-07 16:48:41 +0300

    added about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import About from './components/About';
 import Hero from './components/Hero';
 import Navbar from "./components/Navbar.jsx";
 
@@ -8,6 +9,7 @@ function App() {
       <div>
          <Navbar />
          <Hero />
+         <About />
       </div>
   )
 }
